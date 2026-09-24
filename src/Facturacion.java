1ed8b2940774f58704ae52206ac31b3a2ab9d772
VictorSotelo1511 <victorsotelo1511@gmail.com>
2026-09-23 22:40:56 -0600

    Add Facturacion.java file

--- src/Facturacion.java
+++ src/Facturacion.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Practica4 {
+public class Facturacion {
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa el precio del producto: ");
