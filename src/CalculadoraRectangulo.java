2187e7b205db0453ea0c77dc816522eab805947e
VictorSotelo1511 <victorsotelo1511@gmail.com>
2026-09-23 22:39:39 -0600

    Add CalculadoraRectangulo.java file

--- src/CalculadoraRectangulo.java
+++ src/CalculadoraRectangulo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Practica2 {
+public class CalculadoraRectangulo {
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa la altura: ");
