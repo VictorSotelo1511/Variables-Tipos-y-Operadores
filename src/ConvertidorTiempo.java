eff87dbd7da936905d8533c77062375ae0ca2b40
VictorSotelo1511 <victorsotelo1511@gmail.com>
2026-09-23 22:40:23 -0600

    Add ConvertidorTiempo.java file

--- src/ConvertidorTiempo.java
+++ src/ConvertidorTiempo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Practica3 {
+public class ConvertidorTiempo {
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa los segundos: ");
